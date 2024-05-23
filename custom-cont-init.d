1518caca1b63753408cfea326348723af6370123
Philipp <philipp.lamp@bmw.de>
2024-05-23 20:39:32 +0200

    update

--- custom-cont-init.d
+++ custom-cont-init.d
@@ -1,3 +1,5 @@
 #!/bin/bash
-sed -i 's#	AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+#sed -i 's#	AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+
+echo "AllowTcpForwarding yes" >> /etc/ssh/sshd_config
 
